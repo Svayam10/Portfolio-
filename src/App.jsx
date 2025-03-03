1b97d526dc03d85c553bcec720f499863a30e4bf
Svayam Mehta <svayammehta2004@gmail.com>
2025-03-03 19:48:42 +0530

    Home Page Ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { LoadingScreen } from './components/LoadingScreen.jsx'
 import "./index.css"
 import { useState } from 'react'
 import { Navbar } from './components/Navbar.jsx'
+import { MobileMenu } from './components/MobileMenu.jsx'
+import { Home } from './components/sections/Home.jsx'
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={()=>setIsLoaded(true)} />}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
-        <Navbar/>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> 
+        <Home/>
+
       </div>
     </>
   )
